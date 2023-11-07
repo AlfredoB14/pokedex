f0e896c5695ddc7bf263378eeb431cfc6edce856
AlfredoB14 <90919198+AlfredoB14@users.noreply.github.com>
2023-11-07 12:53:54 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import pokeLogo from './assets/pokeballgym.png'
+import { Pokedex } from './components/pokedex'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://pokeapi.co/" target="_blank">
+          <img src={pokeLogo} className="logo" alt="PokeLogo" />
+        </a>
+      </div>
+      <h1>Pokedex</h1>
+      <div className="card">
+        <Pokedex></Pokedex>
+      </div>
+
+    </>
+  )
+}
+
+export default App
