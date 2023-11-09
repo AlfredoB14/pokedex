f8136b9ab44ebd2b8f07917e51ce210704894037
AlfredoB14 <alfredobarranco14@gmail.com>
2023-11-09 10:08:35 -0600

    Imagenes

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Pokemon } from './Pokemon'
+
 export const Pokedex = () => {
 
     const[pokemones, setPokemones] = useState([])
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import axios from 'axios'
 
 export const Pokemon = ({pokemon}) => {
+
+  const [pkmn, setPkmn] = useState([])
+
+  useEffect(() => {
+    axios.get(pokemon.url).then((response) => {
+        setPkmn(response.data)
+    })
+  }, [setPkmn])
+  
+  let imagen = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pkmn.id}.png`
+
   return (
     <>
         <h3>{pokemon.name}</h3>
+        <img src={imagen} className='ImagenPokemon' alt={pkmn.name}/>
     </>
   )
 }
