22a5e7fa88c39314000b54cb5e9a3afbc36d4f3e
AlfredoB14 <90919198+AlfredoB14@users.noreply.github.com>
2023-11-14 13:09:58 -0600

    Add files via upload

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Pokemon } from './Pokemon'
+import db from '../firebase/firebaseConfig'
+import { collection, addDoc } from 'firebase/firestore'
 export const Pokedex = () => {
 
     const[pokemones, setPokemones] = useState([])
@@ -9,11 +11,25 @@ export const Pokedex = () => {
 
     useEffect(() => {
         axios.get(url).then((response) => {
-            setPokemones(response.data.results)
-            console.log(pokemones)
+            //setPokemones(response.data.results)
+            const pokemonList = response.data.results
+            const pokemonPromises = pokemonList.map(pokemon => {
+                return axios.get(pokemon.url)
+            })
+            Promise.all(pokemonPromises).then(pokemonResponses => {
+                const pokemonData = pokemonResponses.map(res => {
+                    const pokemon = res.data
+                    return {
+                        ...pokemon,
+                        image: pokemon.sprites.front_default,
+                        sprites: pokemon.sprites
+                    }
+                }) 
+                setPokemones(pokemonData)
+            })
         })
     }, [setPokemones])
-
+    
   return ( 
     <div>
         {pokemones.map((pokemon) =>
