f3aadd6a04baf9753ed3629598831f53413167ee
Alfredo <alfredobarranco14@gmail.com>
2023-11-21 16:02:37 -0600

    cambiopantalla

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,39 @@ import './App.css'
 import pokeLogo from './assets/pokeballgym.png'
 import { Pokedex } from './components/Pokedex'
 import ButtonAppBar from './components/Navbar'
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
+import { JoinLeft } from '@mui/icons-material'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [view, setView] = useState(0)
+
 
   return (
     <>
+
       <ButtonAppBar/>
+      
+      <div className='btnGroup'>
+
+        <ButtonGroup variant="outlined" aria-label="outlined primary button group">
+          <Button onClick={() => { setView(1)}}>
+            <img src='/src/assets/grid1.png' height={15} width={15} alt="" />
+          </Button>
+
+          <Button onClick={() => { setView(0)}}>
+            <img src='/src/assets/grid9.png' height={15} width={15} alt="" />
+          </Button>
+        </ButtonGroup>
+        
+      </div>
 
       <div className="card">
-        <Pokedex></Pokedex>
+      
+        {view === 0 ? <Pokedex/> : <h1>Prueba</h1>}
 
       </div>
+
 
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,6 +26,8 @@ export default function ButtonAppBar() {
         </Toolbar>
       </AppBar>
     </Box>
+
+
   );
 
 }
